540f03596db40c4ab5d7e3b41f9394f527e3598a
Oluwadamilola Owolabi <owolabioluwadamilola1@gmail.com>
2023-11-27 04:06:50 -0800

    commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,27 +3,32 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
+/**
+ * create_file - Creates a file.
+ *
+ * @filename: The name of the file to create.
+ * @text_content: A NULL-terminated string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
 int create_file(const char *filename, char *text_content)
+
 {
-    mode_t mode = S_IRUSR | S_IWUSR;
-    FILE *file;
+	mode_t mode = S_IRUSR | S_IWUSR;
+	FILE *file;
 
-    if (filename == NULL) {
-        return -1;
-    }
+	if (filename == NULL)
+	return (-1);
 
-    file = fopen(filename, "w");
-    if (file == NULL) {
-        return -1;
-    }
+	file = fopen(filename, "w");
+	if (file == NULL)
+	return (-1);
 
-    if (text_content != NULL) {
-        fputs(text_content, file);
-    }
+	if (text_content != NULL)
+	fputs(text_content, file);
+	fclose(file);
 
-    fclose(file);
+	chmod(filename, mode);
 
-    chmod(filename, mode);
-
-    return 1;
+	return (1);
 }
